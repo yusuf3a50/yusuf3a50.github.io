818b12f36ad97db6f7a2d07a51513de051501684
yusuf3a50 <ybyphone@disroot.org>
2021-12-11 19:01:04 +0000

    changes to all files

--- listenerButton.js
+++ listenerButton.js
@@ -1,5 +1,13 @@
 function buttonFunction() {
-    console.log("hi")
+    console.log("Please pick me for your course!")
 };
 let clickMeButton = document.querySelector(".button");
 clickMeButton.addEventListener("click", buttonFunction);
+
+function darkModeFunction() {
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+  }
+  let darkModeButton = document.querySelector(".darkModeButton");
+  darkModeButton.addEventListener("click", darkModeFunction);
+  
