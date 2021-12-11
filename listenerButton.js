55b6e611f256be9591c5fc665c57b754eb9af552
yusuf3a50 <ybyphone@disroot.org>
2021-12-11 19:05:01 +0000

    arbitrary changes in JS file

--- listenerButton.js
+++ listenerButton.js
@@ -1,9 +1,3 @@
-function buttonFunction() {
-    console.log("Please pick me for your course!")
-};
-let clickMeButton = document.querySelector(".button");
-clickMeButton.addEventListener("click", buttonFunction);
-
 function darkModeFunction() {
     let element = document.body;
     element.classList.toggle("dark-mode");
@@ -11,3 +5,8 @@ function darkModeFunction() {
   let darkModeButton = document.querySelector(".darkModeButton");
   darkModeButton.addEventListener("click", darkModeFunction);
   
+  function buttonFunction() {
+    console.log("Please pick me for your course!")
+};
+let clickMeButton = document.querySelector(".button");
+clickMeButton.addEventListener("click", buttonFunction);
