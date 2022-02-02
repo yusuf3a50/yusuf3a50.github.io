b90bf946274d019e0f1d3fde09d10d4b91afcbb9
yusuf3a50 <ybyphone@disroot.org>
2022-02-02 14:58:01 +0000

    During testing locally, having the stylesheet named style.css was causing bugs so I changed the stylesheet filename to index.css Also some other minor changes to the website

--- listenerButton.js
+++ listenerButton.js
@@ -7,12 +7,11 @@ function darkModeFunction() {
 let darkModeButton = document.querySelector(".darkModeButton");
 darkModeButton.addEventListener("click", darkModeFunction);
 
-
 //Answer to the meaning of life, the universe and everything button//
 function buttonFunction() {
   let answer = document.getElementById("answer42")
   document.getElementById("answer42").innerText = 42
 };
 
-let clickMeButton = document.querySelector(".funButton");
+let clickMeButton = document.querySelector(".questionButton");
 clickMeButton.addEventListener("click", buttonFunction);
